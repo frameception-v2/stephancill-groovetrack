ead6723041acccae8bd8c61279fb441d34691f1f
hellno <686075+hellno@users.noreply.github.com>
2025-03-31 20:11:50 +0000

    Initial commit from template

---
+++ src/components/Frame.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useCallback, useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "~/components/ui/card";
+
+import { Label } from "~/components/ui/label";
+import { useFrameSDK } from "~/hooks/useFrameSDK";
+
+function ExampleCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Welcome to the Frame Template</CardTitle>
+        <CardDescription>
+          This is an example card that you can customize or remove
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Label>Place content in a Card here.</Label>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default function Frame() {
+  const { isSDKLoaded } = useFrameSDK();
+
+  if (!isSDKLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="w-[300px] mx-auto py-2 px-2">
+      <ExampleCard />
+    </div>
+  );
+}
